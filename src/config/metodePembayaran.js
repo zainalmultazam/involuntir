e54e78dc18724a4f854873dfd7949b116b868703
Naufal-dzaki <muhammadnaufaldzaki261@gmail.com>
2022-12-23 11:22:31 +0700

    add new data bank

--- src/config/metodePembayaran.js
+++ src/config/metodePembayaran.js
@@ -63,6 +63,42 @@ export const METODE_PEMBAYARAN = [
     label: 'Dana',
     image: '/images/icon/metode-pembayaran/dana.svg',
   },
+  {
+    metode: 'emoney',
+    nama: 'jenius',
+    label: 'Jenius',
+    image: '/images/icon/metode-pembayaran/jenius.svg',
+  },
+  {
+    metode: 'emoney',
+    nama: 'bca',
+    label: 'Bank Central Asia',
+    image: '/images/icon/metode-pembayaran/bca.svg',
+  },
+  {
+    metode: 'emoney',
+    nama: 'bni',
+    label: 'Bank Negara Indonesia',
+    image: '/images/icon/metode-pembayaran/bni.svg',
+  },
+  {
+    metode: 'emoney',
+    nama: 'bri',
+    label: 'Bank Rakyat Indonesia',
+    image: '/images/icon/metode-pembayaran/bri.svg',
+  },
+  {
+    metode: 'emoney',
+    nama: 'mandiri',
+    label: 'Bank Mandiri',
+    image: '/images/icon/metode-pembayaran/mandiri.svg',
+  },
+  {
+    metode: 'emoney',
+    nama: 'permata',
+    label: 'Bank Permata',
+    image: '/images/icon/metode-pembayaran/bank-permata.svg',
+  },
   // Dompet Peduly
   {
     metode: 'balance',
